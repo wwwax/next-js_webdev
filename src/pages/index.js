34cab2762e84fd1665923a19896c68412f67c295
Max Ivanov <waxivanov@gmail.com>
2023-12-16 19:29:49 +0200

    feat: add logo and metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import { Heading } from '@/components/Heading';
 import styles from '@/styles/Home.module.scss';
+import Head from 'next/head';
 
 const Home = () => {
   return (
     <div className={styles.wrapper}>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Heading text="Home" />
     </div>
   );
