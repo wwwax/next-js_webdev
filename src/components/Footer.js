8177016dfdc64d8285f119f7bc2b213ec5db760c
Max Ivanov <waxivanov@gmail.com>
2023-12-16 17:57:29 +0200

    feat: add styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Heading } from '../components/Heading';
 export const Footer = () => {
   return (
     <header>
-      <Heading text="created by www4x" />
+      <Heading tag="h3" text="created by www4x" />
     </header>
   );
 };
