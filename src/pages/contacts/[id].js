4db8fcf9c479faa30e884cce6f9199594ef10a78
Max Ivanov <waxivanov@gmail.com>
2023-12-17 11:58:06 +0200

    feat: get contact in the ssr approach

--- src/pages/contacts/[id].js
+++ src/pages/contacts/[id].js
@@ -1,6 +1,29 @@
 import { ContactInfo } from '@/components/ContactInfo';
 import Head from 'next/head';
 
+// this function is for asynchronous requests in the SSR approach
+export const getServerSideProps = async (context) => {
+  const { id } = context.params;
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      contact: data,
+    },
+  };
+};
+
 const Contact = ({ contact }) => {
   return (
     <div>
